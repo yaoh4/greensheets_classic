83fba00ab510a5327baa3e4cd83a47c44cba1ef0
puscask <puscask@nih.gov>
2005-03-25 18:45:40 +0000

    added loading additional xmlsrc files for greensheet submitted before 3/24/05

--- Implementation/GsApp/trunk/src/gov/nih/nci/iscs/numsix/greensheets/utils/AppConfigLoader.java
+++ Implementation/GsApp/trunk/src/gov/nih/nci/iscs/numsix/greensheets/utils/AppConfigLoader.java
@@ -63,6 +63,14 @@ public class AppConfigLoader {
         AppConfigProperties.getInstance().addProperty("PNC_QUESTIONS_SRC", new File(path + "xmlxsltsrc/PNC_Questions.xml"));
         AppConfigProperties.getInstance().addProperty("SC_QUESTIONS_SRC", new File(path + "xmlxsltsrc/SC_Questions.xml"));
         AppConfigProperties.getInstance().addProperty("SNC_QUESTIONS_SRC", new File(path + "xmlxsltsrc/SNC_Questions.xml"));
+        
+        AppConfigProperties.getInstance().addProperty("PC_QUESTIONS_SRC_3.23.05", new File(path + "xmlxsltsrc/PC_Questions_3.23.05.xml"));
+        AppConfigProperties.getInstance().addProperty("PNC_QUESTIONS_SRC_3.23.05", new File(path + "xmlxsltsrc/PNC_Questions_3.23.05.xml"));
+        AppConfigProperties.getInstance().addProperty("SC_QUESTIONS_SRC_3.23.05", new File(path + "xmlxsltsrc/SC_Questions_3.23.05.xml"));
+        AppConfigProperties.getInstance().addProperty("SNC_QUESTIONS_SRC_3.23.05", new File(path + "xmlxsltsrc/SNC_Questions_3.23.05.xml"));
+        
+        
+        
         AppConfigProperties.getInstance().addProperty("FORM_XML_TRANSLATOR", gsFormXmlTranslator);
         AppConfigProperties.getInstance().addProperty("FORM_FO_TRANSLATOR", gsFormToFoTranslator);
 
