570f00e7c0de48a67366dd3306bfc22905109d83
kanchink <kanchink@nih.gov>
2006-04-06 15:46:09 +0000

    Commenting out the method implementation for initPreferences();

--- Implementation/GsApp/trunk/src/gov/nih/nci/iscs/numsix/greensheets/utils/AppConfigLoader.java
+++ Implementation/GsApp/trunk/src/gov/nih/nci/iscs/numsix/greensheets/utils/AppConfigLoader.java
@@ -43,9 +43,9 @@ public class AppConfigLoader {
     }
 
     public static void initPreferences(String path) throws Exception {
-        Properties p = new Properties();
-        p.load(new FileInputStream(path + "preferences.properties"));
-        AppConfigProperties.getInstance().addProperty(GreensheetsKeys.KEY_USER_PREFERENCES, p);
+       // Properties p = new Properties();
+      //  p.load(new FileInputStream(path + "preferences.properties"));
+      //  AppConfigProperties.getInstance().addProperty(GreensheetsKeys.KEY_USER_PREFERENCES, p);
     }
 
 
