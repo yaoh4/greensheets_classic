06693ba8d2e51df67994fe5ae9e0edaaefcfaade
pillaian <pillaian@nih.gov>
2007-04-02 19:43:37 +0000

    bug fix for 4380 iteration # 3 Removing Mechanism

--- Implementation/GsApp/trunk/src/gov/nih/nci/iscs/numsix/greensheets/services/grantmgr/GrantMgrImpl.java
+++ Implementation/GsApp/trunk/src/gov/nih/nci/iscs/numsix/greensheets/services/grantmgr/GrantMgrImpl.java
@@ -304,7 +304,7 @@ public class GrantMgrImpl implements GrantMgr {
 			boolean grantNumberBlank = (grantNumber == null || grantNumber.equals(null) || grantNumber.equals(""));
 			boolean lastNameBlank = (lastName == null || lastName.equals(null) || lastName.equals(""));
 			boolean firstNameBlank = (firstName == null || firstName.equals(null) || firstName.equals(""));
-			boolean mechanismBlank = (mechanism == null || mechanism.equals(null) || mechanism.equals(""));
+//			boolean mechanismBlank = (mechanism == null || mechanism.equals(null) || mechanism.equals(""));
 			
 			logger.debug("Grant Number : "+grantNumberBlank +" Last Name : " + lastNameBlank +" First Name : " + firstNameBlank);
 			ViewDataRetrieverFactory drFactory = new ViewDataRetrieverFactory();
@@ -312,7 +312,7 @@ public class GrantMgrImpl implements GrantMgr {
 					.getViewDataRetriever("FORM_GRANT_VW");
 
 			// add conditions here...
-			if (mechanismBlank && grantNumberBlank && lastNameBlank && firstNameBlank) {
+			if (grantNumberBlank && lastNameBlank && firstNameBlank) {
 				logger.debug("Competing grants ");
 				addBaseFilterCriteria(viewDataRetriever, user);
 				addLatestBudgetStartDateCriteria(viewDataRetriever);
@@ -345,7 +345,7 @@ public class GrantMgrImpl implements GrantMgr {
 //				Now retrieve the Non-Competing grants.
 				viewDataRetriever.clearConditions();
 				
-				if (mechanismBlank && grantNumberBlank && lastNameBlank && firstNameBlank) {
+				if (grantNumberBlank && lastNameBlank && firstNameBlank) {
 					logger.debug("Non Competing grants ");
 					addBaseFilterCriteria(viewDataRetriever, user);
 					addLatestBudgetStartDateCriteria(viewDataRetriever);
