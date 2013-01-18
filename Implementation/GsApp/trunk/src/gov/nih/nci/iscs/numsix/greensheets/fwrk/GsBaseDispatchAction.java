154cd0ada7e8b45986e26ec3748ebb44ab1c4e45
kouzneta <kouzneta@nih.gov>
2013-01-18 15:49:30 +0000

    Renaming what used to be trunk to pre_2-5_trunk because what's in it is the original Greensheets code without Spring that used to run in JRun. We will now make the content of what was branch 2.5 the new trunk.

--- Implementation/GsApp/trunk/src/gov/nih/nci/iscs/numsix/greensheets/fwrk/GsBaseDispatchAction.java
+++
@@ -1,7 +0,0 @@
-package gov.nih.nci.iscs.numsix.greensheets.fwrk;
-
-import org.apache.struts.actions.DispatchAction;
-
-public class GsBaseDispatchAction extends DispatchAction {
-
-}
--- Implementation/GsApp/trunk/src/gov/nih/nci/iscs/numsix/greensheets/fwrk/GsBaseLookupDispatchAction.java
+++
@@ -1,7 +0,0 @@
-package gov.nih.nci.iscs.numsix.greensheets.fwrk;
-
-import org.apache.struts.actions.LookupDispatchAction;
-
-public abstract class GsBaseLookupDispatchAction extends LookupDispatchAction {
-
-}
