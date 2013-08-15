f3164b30b462c41d19c3f4b869ad5562d69c9ba6
zhaos3 <zhaos3@nih.gov>
2013-08-15 20:51:06 +0000

    GREENSHEETS-353

--- Implementation/GsApp/trunk/src/gov/nih/nci/iscs/numsix/greensheets/services/GreensheetFormService.java
+++ Implementation/GsApp/trunk/src/gov/nih/nci/iscs/numsix/greensheets/services/GreensheetFormService.java
@@ -4,6 +4,7 @@ import gov.nih.nci.iscs.numsix.greensheets.fwrk.GreensheetBaseException;
 import gov.nih.nci.iscs.numsix.greensheets.services.greensheetformmgr.GreensheetFormProxy;
 
 public interface GreensheetFormService {
-	public GreensheetFormProxy getGreensheetForm(FormGrantProxy formGrantProxy, String formRoleCode) 
-			throws GreensheetBaseException;
+    public GreensheetFormProxy getGreensheetForm(FormGrantProxy formGrantProxy, String formRoleCode)
+            throws GreensheetBaseException;
+
 }
