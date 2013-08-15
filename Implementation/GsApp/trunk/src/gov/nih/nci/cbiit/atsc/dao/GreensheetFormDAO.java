062e473ec713ce10a0b453191d0166c31007d7fe
zhaos3 <zhaos3@nih.gov>
2013-08-15 20:48:59 +0000

    GREENSHEETS-353

--- Implementation/GsApp/trunk/src/gov/nih/nci/cbiit/atsc/dao/GreensheetFormDAO.java
+++ Implementation/GsApp/trunk/src/gov/nih/nci/cbiit/atsc/dao/GreensheetFormDAO.java
@@ -3,5 +3,10 @@ package gov.nih.nci.cbiit.atsc.dao;
 import gov.nih.nci.iscs.numsix.greensheets.fwrk.GreensheetBaseException;
 
 public interface GreensheetFormDAO {
-	public GreensheetForm getGreensheetForm(FormGrant grant, String formtype) throws GreensheetBaseException;
+    public GreensheetForm getGreensheetForm(FormGrant grant, String formtype) throws GreensheetBaseException;
+
+    public boolean checkActionStatusByApplId(String applId) throws GreensheetBaseException;
+
+    public boolean checkActionStatusByGrantId(String grantId) throws GreensheetBaseException;
+
 }
